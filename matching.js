94ffda48a561ca086b433120f73ba1d2253ab5e2
willshallC <netcreaterss@gmail.com>
2023-03-17 12:53:16 +0530

    Half Completed

--- matching.js
+++ matching.js
@@ -4,10 +4,41 @@ let cardOne, cardTwo;
 
 function flipCard(e){
     let clickedCard = e.target;
-    clickedCard.classList.add("flip");
-    cardOne = clickedCard;
+
+    if(clickedCard !== cardOne){
+        clickedCard.classList.add("flip");
+        if(!cardOne){
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+
+        let cardOneImg = cardOne.querySelector("img").src;
+        let cardTwoImg = cardTwo.querySelector("img").src;
+
+        matchedCards(cardOneImg,cardTwoImg);
+    }
 }
 
 cards.forEach(card=>{
     card.addEventListener('click', flipCard);
 })
+
+function matchedCards(img1, img2){
+    if(img1 === img2){
+        cardOne.removeEventListener('click',flipCard);
+        cardTwo.removeEventListener('click',flipCard);
+        cardOne = cardTwo = "";
+        return;
+    }
+
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");    
+    }, 400);
+    
+    setTimeout(() => {
+        cardOne.classList.remove("shake","flip");
+        cardTwo.classList.remove("shake","flip");
+        cardOne = cardTwo = "";    
+    }, 1200);
+}
