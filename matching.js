e21d93f427144d0034e5f2760257aeea684a2d81
willshallC <netcreaterss@gmail.com>
2023-03-17 15:25:41 +0530

    Matching game 99% Completed

--- matching.js
+++ matching.js
@@ -1,17 +1,19 @@
 const cards = document.querySelectorAll(".cards");
 
+let matchedCard = 0;
 let cardOne, cardTwo;
+let disableDeck = false;
 
 function flipCard(e){
     let clickedCard = e.target;
 
-    if(clickedCard !== cardOne){
+    if(clickedCard !== cardOne && !disableDeck){
         clickedCard.classList.add("flip");
         if(!cardOne){
             return cardOne = clickedCard;
         }
         cardTwo = clickedCard;
-
+        disableDeck = true;
         let cardOneImg = cardOne.querySelector("img").src;
         let cardTwoImg = cardTwo.querySelector("img").src;
 
@@ -25,10 +27,16 @@ cards.forEach(card=>{
 
 function matchedCards(img1, img2){
     if(img1 === img2){
+        matchedCard++;
+        if(matchedCard == 8){
+           setTimeout(()=>{
+            return shuffleCard();
+           },1000);
+        }
         cardOne.removeEventListener('click',flipCard);
         cardTwo.removeEventListener('click',flipCard);
         cardOne = cardTwo = "";
-        return;
+        return disableDeck = false;
     }
 
     setTimeout(() => {
@@ -39,6 +47,16 @@ function matchedCards(img1, img2){
     setTimeout(() => {
         cardOne.classList.remove("shake","flip");
         cardTwo.classList.remove("shake","flip");
-        cardOne = cardTwo = "";    
+        cardOne = cardTwo = "";   
+        disableDeck = false; 
     }, 1200);
+}
+
+function shuffleCard(){
+    matchedCard = 0;
+    cardOne = cardTwo = "";
+    cards.forEach(card=>{
+        card.classList.remove("flip");
+        card.addEventListener('click', flipCard);
+    })
 }
