aef268b0628b8da29e03f095659ca8eb0879628b
willshallC <netcreaterss@gmail.com>
2023-04-21 10:10:59 +0530

    form structure

--- Laravel/edit.blade.php
+++ Laravel/edit.blade.php
@@ -17,3 +17,44 @@
     {{ session('status') }}
 </div>
 @endif
+
+<div class="row">
+    <div class="col-md-8 edit_profile-admin">
+        <div class="card edit_password">
+            <div class="card-header">
+                <h5 class="title">{{ __('Edit Profile') }}</h5>
+            </div>
+            <form method="post" action="{{ route('profile.update') }}" autocomplete="off" enctype="multipart/form-data">
+                <div class="card-body">
+                    @csrf
+                    @method('put')
+
+                    <div class="form-group img-sec-profile-sec">
+                        <div class="img-sec-profile-inner form-control">
+                            <img id="profile-pic" class="img-responsive" src="{{ asset('/storage') }}/{{ auth()->user()->profile_pic }}">
+                            <input type="file" id="profile-pic-input" class="form-control" name="photo[]">
+                        </div>
+                    </div>
+
+
+                    <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                        <label>{{ __('Name') }}</label>
+                        <input type="text" name="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}" value="{{ old('name', auth()->user()->name) }}">
+                        @include('alerts.feedback', ['field' => 'name'])
+                    </div>
+
+                    <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                        <label>{{ __('Email address') }}</label>
+                        <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email address') }}" value="{{ old('email', auth()->user()->email) }}">
+                        @include('alerts.feedback', ['field' => 'email'])
+                    </div>
+
+                    <button type="submit" class="btn btn-fill btn-primary">{{ __('Save') }}</button>
+                </div>
+                    <!--div class="card-footer">
+                      
+                    </div-->
+                </form>
+            </div>
+
+        </br>
