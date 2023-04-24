a38a0fb53f7c1d798d13d7dc2e2d47d70b1a1ba1
willshallC <netcreaterss@gmail.com>
2023-04-24 10:19:11 +0530

    Defined arguemts for CPT

--- WordPress/custom_post_type.php
+++ WordPress/custom_post_type.php
@@ -9,5 +9,28 @@
             'view_item' => 'View Articles',
             
         );
+
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'has_archive' => true,
+            'show_ui' => true,
+            'capability_type' => 'post',
+            'hierarchical' => false,
+            'rewrite' => array('slug' => 'articles'),
+            'query_var' => true,
+            'supports' => array(
+                'title',
+                'editor',
+                'excerpt',
+                'trackbacks',
+                'custom-fields',
+                'comments',
+                'revisions',
+                'thumbnail',
+                'author',
+                'page-attributes'
+            )
+        );
     }
 ?>
