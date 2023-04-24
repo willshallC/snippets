2a946802b0ba6e79b468b0ded9d909e7cc5db049
willshallC <netcreaterss@gmail.com>
2023-04-24 10:18:20 +0530

    Labels for new Post Type

--- WordPress/custom_post_type.php
+++ WordPress/custom_post_type.php
@@ -1,3 +1,13 @@
 <?php 
-    function article_init(){}
+    function article_init(){
+        $labels = array(
+            'name' => 'Articles',
+            'singular_name' => 'Article',
+            'add_new' => 'Add Article',
+            'edit_item' => 'Edit Article',   
+            'all_items' => 'All Articles',
+            'view_item' => 'View Articles',
+            
+        );
+    }
 ?>
