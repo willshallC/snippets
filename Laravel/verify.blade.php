284a123998d74919c92607c0ac56538d9b2edbad
willshallC <netcreaterss@gmail.com>
2023-04-26 10:12:58 +0530

    Email address verification

---
+++ Laravel/verify.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app', ['class' => 'bg-default'])
+
+@section('content')
+    @include('layouts.headers.guest')
+
+    <div class="container mt--8 pb-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-7">
+                <div class="card bg-secondary shadow border-0">
+                    <div class="card-body px-lg-5 py-lg-5">
+                        <div class="text-center text-muted mb-4">
+                            <small>{{ __('Verify Your Email Address') }}</small>
+                        </div>
