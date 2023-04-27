82cc5accadc0cd761e30fa7169af67cc3d0dd73b
willshallC <netcreaterss@gmail.com>
2023-04-27 10:12:53 +0530

    Display parent categories

--- WordPress/child_category_posts.php
+++ WordPress/child_category_posts.php
@@ -1,1 +1,11 @@
 <?php $cat = get_terms(array('taxonomy'=>'articles_category', 'hide_empty'=>false, 'parent'=>0, 'hierarchical'=>1)); //0 no?>
+
+<div class="cat-main">
+<?php 
+
+    foreach($cat as $pcat){ ?>
+        <?php
+            $subcat = get_terms(array('taxonomy'=>'articles_category', 'parent'=>$pcat->term_id));
+        ?>
+            <div class="parent">
+                <h3 class="parent-cat" id=<?php echo $pcat->term_id; ?>><?php echo $pcat->name; ?></h3>
