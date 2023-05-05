10927f75c23e799350f2614ac4ee48c12c2d1ab1
willshallC <netcreaterss@gmail.com>
2023-05-05 12:44:08 +0530

    get view coutns

--- WordPress/popular_post.php
+++ WordPress/popular_post.php
@@ -1,6 +1,16 @@
 <?php 
 
     function count_post_visits(){
-        if(is_single()){}
+        if(is_single()){
+            global $post;
+            $views = get_post_meta( $post->ID, 'my_post_viewed', true );
+            if( $views == '' ) {
+                update_post_meta( $post->ID, 'my_post_viewed', '1' );   
+            } else {
+                $views_no = intval( $views );
+                update_post_meta( $post->ID, 'my_post_viewed', ++$views_no );
+            }
+        }
+        
     }
 ?>
