91501584d119f5daf51dcc0920d85eb67dd36428
willshallC <netcreaterss@gmail.com>
2023-05-08 10:07:57 +0530

    plugin description

--- WordPress/icon_plugin.php
+++ WordPress/icon_plugin.php
@@ -1,2 +1,10 @@
 <?php
-    
+
+    /*
+        Plugin Name: Single Custom Icon
+        Plugin URI: https://dev.redefiningweb.com
+        Description: Icons Plugin
+        Author: Gurpreet
+        Author URI: https://dev.redefiningweb.com
+        Version: 1.0
+    */
