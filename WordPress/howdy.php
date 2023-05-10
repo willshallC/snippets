fdacafc3db721db2ea041a43c605af643107a246
willshallC <netcreaterss@gmail.com>
2023-05-10 10:55:58 +0530

    change 'howdy, user_name' to custom text

--- WordPress/howdy.php
+++ WordPress/howdy.php
@@ -2,7 +2,7 @@
 
 
     function replace_howdy( $wp_admin_bar ){
-        $my_account= $wp_admin_bar-&;get_node('my-account');
+        $my_account= $wp_admin_bar-&gt;get_node('my-account');
         $newtitle = str_replace( 'Howdy,', 'Logged in as', $my_account-&gt;title );
         $wp_admin_bar-&gt;add_node( array(
             'id' =&gt; 'my-account',
@@ -10,5 +10,6 @@
         ) );
                     
     }
+    add_filter( 'admin_bar_menu', 'replace_howdy',25 );
 
 ?>
