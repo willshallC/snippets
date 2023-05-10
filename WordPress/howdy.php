395c2e6e2334754d21f5452517ae2a6cb3652131
willshallC <netcreaterss@gmail.com>
2023-05-10 10:46:19 +0530

    new title

--- WordPress/howdy.php
+++ WordPress/howdy.php
@@ -1,5 +1,8 @@
 <?php 
 
-    function replace_howdy( $wp_admin_bar ){}
+    function replace_howdy( $wp_admin_bar ){
+        $my_account=$wp_admin_bar-&gt;get_node('my-account');
+        $newtitle = str_replace( 'Howdy,', 'Logged in as', $my_account-&gt;title );
+    }
 
 ?>
