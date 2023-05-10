f5ca52f27935be7199aa1a0e144e26629e4048c4
willshallC <netcreaterss@gmail.com>
2023-05-10 10:54:49 +0530

    add_node

--- WordPress/howdy.php
+++ WordPress/howdy.php
@@ -1,8 +1,14 @@
 <?php 
 
+
     function replace_howdy( $wp_admin_bar ){
-        $my_account=$wp_admin_bar-&gt;get_node('my-account');
+        $my_account= $wp_admin_bar-&;get_node('my-account');
         $newtitle = str_replace( 'Howdy,', 'Logged in as', $my_account-&gt;title );
+        $wp_admin_bar-&gt;add_node( array(
+            'id' =&gt; 'my-account',
+            'title' =&gt; $newtitle,
+        ) );
+                    
     }
 
 ?>
