d83e12f8f0e1c49804297f1fe14d3ef67f53387b
willshallC <netcreaterss@gmail.com>
2023-05-11 10:23:49 +0530

    defining hook

--- WordPress/categories_on_cf7.php
+++ WordPress/categories_on_cf7.php
@@ -1,3 +1,8 @@
 <?php
     add_action( 'wpcf7_init', 'custom_add_form_tag_customlist' );
+
+    function custom_add_form_tag_customlist() {
+        wpcf7_add_form_tag( array( 'customlist', 'customlist*' ), 
+    'custom_customlist_form_tag_handler', true );
+    }
 ?>
