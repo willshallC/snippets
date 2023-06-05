058738de4ededd21d3fd2e8c1563e26eb07f9da6
willshallC <netcreaterss@gmail.com>
2023-06-05 15:36:12 +0530

    added pagination

--- WordPress/ajax_pagination.php
+++ WordPress/ajax_pagination.php
@@ -20,3 +20,14 @@
 
     ?>
 </div>
+<div id="ajax-pagination">
+    <?php $big = 999999999; // need an unlikely integer
+		 echo paginate_links( array(
+			'base' => str_replace( $big, '%#%', esc_url(get_pagenum_link( $big ) )),
+			'format' => '?paged=%#%',
+			'current' => max( 1, get_query_var('paged') ),
+			'total' => $query->max_num_pages,
+			'prev_text'    => __('‹'),
+			'next_text'    => __('›'),
+		) ); ?>
+</div>
