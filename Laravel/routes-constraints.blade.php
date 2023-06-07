21898b98a3019371b4ce5dd56df048ace35407d9
willshallC <netcreaterss@gmail.com>
2023-06-07 18:33:35 +0530

    constraints for numeric values

--- Laravel/routes-constraints.blade.php
+++ Laravel/routes-constraints.blade.php
@@ -10,4 +10,14 @@
     }
 })->whereIn('id',['on1','chain','Hello']);
 
+//only numeric values
+Route::get('/post/{id?}',function($id=null){
+    if($id){
+        return "<h1>POST ID: $id</h1>";
+    }
+    else{
+        return "<h1>No POST ID</h1>";
+    }
+})->whereNumeric('id');
+
 ?>
