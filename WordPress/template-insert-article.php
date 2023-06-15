0f187ded161ba3f268cfe57b266c8c956504b6e8
willshall <netcreaterss@gmail.com>
2023-06-15 18:37:14 +0530

    Insert Post and Taxonomy Form Website

--- WordPress/template-insert-article.php
+++ WordPress/template-insert-article.php
@@ -1,8 +1,8 @@
 <?php 
 	//Template Name: Add Article
 	get_header();
-
-    if(isset($_POST['save_article'])){
+	
+	if(isset($_POST['save_article'])){
 		
 		$id=wp_insert_post(  
 				array(
@@ -12,6 +12,8 @@
 					'post_content'=>$_POST['ades']					
 				)			
 		);
+		
+		wp_set_object_terms( $id, $_POST['articleCat'], 'article_category'  );
 		
 	}
 ?>
@@ -23,11 +25,36 @@
 		 
 			Article Description:<br>
 				<textarea name="ades"></textarea><br><br>
-									
+				
+				<select name="articleCat" style="width:300px; height:40px;">
+					<option>Select Article Category</option>					
+					
+					<?php					
+						$artical_cat = get_terms(array('taxonomy'=>'article_category','hide_empty'=>false,
+	
+						'orderby'=>'name',
+						'order'=>'DESC',
+						'parent'=> 0
+						
+						));
+						
+						foreach($artical_cat as $artical_cat_value){
+							
+						?>
+					
+						<option value="<?php echo $artical_cat_value->name ?>">
+						<?php echo $artical_cat_value->name ?>
+						</option>
+					
+					<?php } ?>
+						
+					
+				</select><br><br>
+					
 			<input type="submit" name="save_article" value="Save Article"/>		
 	</form>
 </div>
 
 <?php
-    get_footer();
+	get_footer();
 ?>
