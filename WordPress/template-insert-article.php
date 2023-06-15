bb8740e98188f24a360a5505db1d17f77289c359
willshall <netcreaterss@gmail.com>
2023-06-15 18:36:22 +0530

    insert post using wp_insert_post function

--- WordPress/template-insert-article.php
+++ WordPress/template-insert-article.php
@@ -1,7 +1,32 @@
 <?php 
 	//Template Name: Add Article
 	get_header();
+
+    if(isset($_POST['save_article'])){
+		
+		$id=wp_insert_post(  
+				array(
+					'post_type'=>'Article',
+					'post_status'=>'draft',
+					'post_title'=>$_POST['atitle'],
+					'post_content'=>$_POST['ades']					
+				)			
+		);
+		
+	}
 ?>
+
+<div>
+	<form method="post">		 
+			Article Title:<br>
+				<input type="text" name="atitle"/><br><br>
+		 
+			Article Description:<br>
+				<textarea name="ades"></textarea><br><br>
+									
+			<input type="submit" name="save_article" value="Save Article"/>		
+	</form>
+</div>
 
 <?php
     get_footer();
