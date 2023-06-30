dc78c99fe041ee46630246557d03ab404a37ab42
willshall <netcreaterss@gmail.com>
2023-06-30 18:51:38 +0530

    Check if there are any tags

--- WordPress/tags-in-draopdown-lish.php
+++ WordPress/tags-in-draopdown-lish.php
@@ -2,3 +2,15 @@
 function app_post_tags_dropdown() {
     // Get all post tags
     $tags = get_tags();
+
+    // Check if there are any tags
+    if ( $tags ) {
+        $output = '<select name="app_post_tags" id="app-post-tags">';
+        $output .= '<option value="">Select a tag</option>';
+
+        foreach ( $tags as $tag ) {
+            $tag_link = get_tag_link( $tag->term_id );
+            $output .= '<option value="' . $tag_link . '">' . $tag->name . '</option>';
+        }
+
+        $output .= '</select>';
