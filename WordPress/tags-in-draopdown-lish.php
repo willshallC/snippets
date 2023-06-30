6d85db0fa968fc9d2004a1319e1f9ebbaa10d402
willshall <netcreaterss@gmail.com>
2023-06-30 18:54:19 +0530

    tags in dropdawn list

--- WordPress/tags-in-draopdown-lish.php
+++ WordPress/tags-in-draopdown-lish.php
@@ -14,3 +14,32 @@ function app_post_tags_dropdown() {
         }
 
         $output .= '</select>';
+
+        // Enqueue JavaScript
+        $output .= '<script>
+            var selectTag = document.getElementById("app-post-tags");
+
+            // Retrieve the stored value
+            var storedValue = localStorage.getItem("app_post_tags_dropdown");
+
+            if (storedValue) {
+                selectTag.value = storedValue;
+            }
+
+            selectTag.onchange = function() {
+                var url = this.value;
+                if (url) {
+                    window.location.href = url;
+                }
+
+                // Store the selected value
+                localStorage.setItem("app_post_tags_dropdown", this.value);
+            };
+        </script>';
+
+        return $output;
+    }
+
+    return 'No tags found.';
+}
+add_shortcode( 'app_post_tags_dropdown', 'app_post_tags_dropdown' );
