27b44326d89c9a3d3d7ed833517630ce87d16840
willshall <netcreaterss@gmail.com>
2023-07-07 18:25:00 +0530

    social share page

--- WordPress/social-share-plugin/social-share-plugin.php
+++ WordPress/social-share-plugin/social-share-plugin.php
@@ -11,3 +11,21 @@ function social_share_menu_item(){
   add_submenu_page("options-general.php", "Social Share", "Social Share", "manage_options", "social-share", "social_share_page"); 
 }
 add_action("admin_menu", "social_share_menu_item");
+
+function social_share_page(){
+   ?>
+      <div class="wrap">
+         <h1>Social Sharing Options</h1>
+ 
+         <form method="post" action="options.php">
+            <?php
+               settings_fields("social_share_config_section");
+ 
+               do_settings_sections("social-share");
+                
+               submit_button(); 
+            ?>
+         </form>
+      </div>
+   <?php
+}
