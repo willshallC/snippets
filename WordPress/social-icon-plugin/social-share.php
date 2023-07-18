444e91d71674936156e9dc9bc989628cb825f0df
willshall <netcreaterss@gmail.com>
2023-07-18 18:25:10 +0530

    add whatsapp and email icons

--- WordPress/social-icon-plugin/social-share.php
+++ WordPress/social-icon-plugin/social-share.php
@@ -1,26 +1,26 @@
 <?php
 /*
-Plugin Name: Social Icons
-Description: Adds Social Media icon using shortcode anywhere.
+Plugin Name: Social Icons Plugin
+Description: Adds Social Media icon using shortcode
 Version: 1.0
-Author: Ayush Thakur
-Author URI: https://www.willshall.com
+Author: Ravi Thakur
+Author URI: https://ravi.redefiningweb.com/
 */
 
 
 // Enqueue Font Awesome CSS, JS, and custom CSS files from CDN
-function enqueue_font_awesome() {
+function enqueue_font_awesome_file() {
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css', array(), '5.15.3');
     wp_enqueue_script('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js', array(), '5.15.3');
-    wp_enqueue_style('social-icon', plugin_dir_url(__FILE__) . 'social-icon.css', array(), '1.0.0');
+    wp_enqueue_style('social-icon', plugin_dir_url(__FILE__) . 'social-share.css', array(), '1.0.0');
 }
-add_action('wp_enqueue_scripts', 'enqueue_font_awesome');
+add_action('wp_enqueue_scripts', 'enqueue_font_awesome_file');
 
 
 // Create a table in the database when the plugin is activated
-function social_icons_activate() {
+function social_icon_activate() {
     global $wpdb;
-    $table_name = $wpdb->prefix . 'socialicons';
+    $table_name = $wpdb->prefix . 'icons';
     $charset_collate = $wpdb->get_charset_collate();
 
     $sql = "CREATE TABLE $table_name (
@@ -33,27 +33,26 @@ function social_icons_activate() {
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta($sql);
 }
-register_activation_hook(__FILE__, 'social_icons_activate');
-
+register_activation_hook(__FILE__, 'social_icon_activate');
 
 // Add menu page in the WordPress dashboard
-function social_icon_menu() {
+function social_icons_menu() {
     add_menu_page(
-        'Social Icons',
-        'Social Icons',
+        'Social Icons Plugin',
+        'Social Icons Plugin',
         'manage_options',
         'social_icon_menu',
-        'social_icon_page',
-        'dashicons-share',
-        20
+        'social_icons_page',
+        'dashicons-admin-plugins',
+        25
     );
 }
-add_action('admin_menu', 'social_icon_menu');
+add_action('admin_menu', 'social_icons_menu');
 
 // Callback function to display the table form
-function social_icon_page() {
+function social_icons_page() {
     global $wpdb;
-    $table_name = $wpdb->prefix . 'socialicons';
+    $table_name = $wpdb->prefix . 'icons';
 
     // Get existing data from the database
     $links = $wpdb->get_results("SELECT title, url FROM $table_name");
@@ -66,7 +65,9 @@ function social_icon_page() {
 
     // Initialize the URLs with empty values
     $facebook_url = '';
-    $instagram_url = '';
+    $instagram_url = '';	
+	$whatsaap_url = '';
+	$email_url = '';
     $twitter_url = '';
 	$linkedin_url ='';
 
@@ -79,13 +80,24 @@ function social_icon_page() {
         $instagram_url = $existing_urls['Instagram'];
     }
 
-    if (isset($existing_urls['Twitter'])) {
+
+	if (isset($existing_urls['Whatsaap'])) {
+        $whatsaap_url = $existing_urls['Whatsaap'];
+    }
+	
+	if (isset($existing_urls['Email'])) {
+        $email_url = $existing_urls['Email'];
+    }
+	
+ 
+   if (isset($existing_urls['Twitter'])) {
         $twitter_url = $existing_urls['Twitter'];
     }
 	
 	if (isset($existing_urls['Linkedin'])) {
         $linkedin_url = $existing_urls['Linkedin'];
     }
+
     ?>
     <div class="wrap">
         <h1>Social Share</h1>
@@ -112,6 +124,16 @@ function social_icon_page() {
                         <td><strong>Instagram</strong></td>
                         <td><input type="text" name="instagram_url" value="<?php echo esc_attr($instagram_url); ?>"></td>
                     </tr>
+					
+					 <tr>
+                        <td><strong>Whatsaap</strong></td>
+                        <td><input type="text" name="whatsaap_url" value="<?php echo esc_attr($whatsaap_url); ?>"></td>
+                    </tr>
+					<tr>
+                        <td><strong>Email</strong></td>
+                        <td><input type="text" name="email_url" value="<?php echo esc_attr($email_url); ?>"></td>
+                    </tr>
+									
                     <tr>
                         <td><strong>Twitter</strong></td>
                         <td><input type="text" name="twitter_url" value="<?php echo esc_attr($twitter_url); ?>"></td>
@@ -120,27 +142,29 @@ function social_icon_page() {
                         <td><strong>Linkedin</strong></td>
                         <td><input type="text" name="linkedin_url" value="<?php echo esc_attr($linkedin_url); ?>"></td>
                     </tr>
+					
                 </tbody>
             </table>
             <?php submit_button('Save Links'); ?>
         </form>
 		
-		<div class="shortcode">
-			<h3 style="color:red;font-size:20px;font-weight:600;"><strong>Note: Use this shortcode <span style="background-color:lightgrey;padding:5px;">[social_share_icons]</span> to display social icons Anywhere.</strong></h3>			
+		<div class="short-code">
+			<h4><strong>Note: Use this shortcode <span style="color:blue;"> [social_media_icons] </span></strong></h4>			
 		</div>
 		
     </div>
     <?php
 }
 
-
 // Save form data to the database
-function social_share_save_data() {
-    if (isset($_POST['facebook_url']) && isset($_POST['instagram_url']) && isset($_POST['twitter_url']) && isset($_POST['linkedin_url'])) {
+function save_data() {
+    if (isset($_POST['facebook_url']) && isset($_POST['instagram_url']) /*&& isset($_POST['twitter_url']) && isset($_POST['linkedin_url']) */) {
         global $wpdb;
-        $table_name = $wpdb->prefix . 'socialicons';
+        $table_name = $wpdb->prefix . 'icons';
         $facebook_url = sanitize_text_field($_POST['facebook_url']);
-        $instagram_url = sanitize_text_field($_POST['instagram_url']);
+        $instagram_url = sanitize_text_field($_POST['instagram_url']);		
+		$whatsaap_url = sanitize_text_field($_POST['whatsaap_url']);
+        $email_url = sanitize_text_field($_POST['email_url']);
         $twitter_url = sanitize_text_field($_POST['twitter_url']);
         $linkedin_url = sanitize_text_field($_POST['linkedin_url']);
 
@@ -179,7 +203,51 @@ function social_share_save_data() {
             );
         }
 
-        if ($wpdb->get_var("SELECT COUNT(*) FROM $table_name WHERE title = 'Twitter'")) {
+		
+		
+	
+	
+	if ($wpdb->get_var("SELECT COUNT(*) FROM $table_name WHERE title = 'Whatsaap'")) {
+            $wpdb->update(
+                $table_name,
+                array('url' => $whatsaap_url),
+                array('title' => 'Whatsaap')
+            );
+        } else {
+            // Insert a new row for Whatsaap if it doesn't exist
+            $wpdb->insert(
+                $table_name,
+                array(
+                    'title' => 'Whatsaap',
+                    'url' => $whatsaap_url
+                )
+            );
+        }
+		
+	
+		
+	if ($wpdb->get_var("SELECT COUNT(*) FROM $table_name WHERE title = 'Email'")) {
+		$wpdb->update(
+			$table_name,
+			array('url' => $email_url),
+			array('title' => 'Email')
+		);
+	} else {
+		// Insert a new row for Email if it doesn't exist
+		$wpdb->insert(
+			$table_name,
+			array(
+				'title' => 'Email',
+				'url' => $email_url
+			)
+		);
+	}
+
+
+
+
+	     
+	   if ($wpdb->get_var("SELECT COUNT(*) FROM $table_name WHERE title = 'Twitter'")) {
             $wpdb->update(
                 $table_name,
                 array('url' => $twitter_url),
@@ -195,6 +263,8 @@ function social_share_save_data() {
                 )
             );
         }
+		
+	
 		
 		if ($wpdb->get_var("SELECT COUNT(*) FROM $table_name WHERE title = 'Linkedin'")) {
             $wpdb->update(
@@ -218,13 +288,14 @@ function social_share_save_data() {
         exit;
     }
 }
-add_action('admin_post_social_share_save_data', 'social_share_save_data');
+add_action('admin_post_social_share_save_data', 'save_data');
+
 
 
 // Create shortcode for social media icons
-function social_share_shortcode($atts) {
+function shortcode($atts) {
     global $wpdb;
-    $table_name = $wpdb->prefix . 'socialicons';
+    $table_name = $wpdb->prefix . 'icons';
 
     $links = $wpdb->get_results("SELECT title, url FROM $table_name");
 
@@ -237,17 +308,28 @@ function social_share_shortcode($atts) {
             // Assign icon classes based on the title
             switch ($link->title) {
                 case 'Facebook':
-                    $icon_class = 'fab fa-facebook-f'; // Replace with the appropriate Font Awesome class
+                    $icon_class = 'fab fa-facebook-f'; 
                     break;
                 case 'Instagram':
-                    $icon_class = 'fab fa-instagram'; // Replace with the appropriate Font Awesome class
+                    $icon_class = 'fab fa-instagram'; 
                     break;
+					
+					
+				case 'Whatsaap':
+                    $icon_class = 'fab fa-whatsapp'; 
+                    break;
+                case 'Email':
+                    $icon_class = 'far fa-envelope';
+                    break;
+					
+					
                 case 'Twitter':
-                    $icon_class = 'fab fa-twitter'; // Replace with the appropriate Font Awesome class
+                    $icon_class = 'fab fa-twitter'; 
                     break;
                 case 'Linkedin':
-                    $icon_class = 'fab fa-linkedin-in'; // Replace with the appropriate Font Awesome class
+                    $icon_class = 'fab fa-linkedin-in'; 
                     break;
+					
                 // Add more cases for other social media titles
             }
 
@@ -257,4 +339,5 @@ function social_share_shortcode($atts) {
 
     return $output;
 }
-add_shortcode('social_share_icons', 'social_share_shortcode');
+add_shortcode('social_media_icons', 'shortcode');
+
