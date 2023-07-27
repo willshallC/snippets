470df1b775255835092c460adf163066dabe8ff4
willshall <netcreaterss@gmail.com>
2023-07-27 18:32:38 +0530

    Get admin email address

--- WordPress/post-update-notification.php
+++ WordPress/post-update-notification.php
@@ -11,12 +11,13 @@ function send_post_update_notification($new_status, $old_status, $post) {
         $message = 'The following post has been updated on your website:' . "\n\n";
         $message .= 'Title: ' . $post->post_title . "\n";
         $message .= 'Link: ' . get_permalink($post->ID) . "\n";
-        // Get the admin email address
-        $admin_email = get_option('admin_email');
+       
+        // Get the admin email address 
+        $admin_email = get_option('admin_email'); 
         // Send the email to the admin
         wp_mail($admin_email, $subject, $message);
     }
-
+    
 }
 
 add_action('transition_post_status', 'send_post_update_notification', 10, 3);
