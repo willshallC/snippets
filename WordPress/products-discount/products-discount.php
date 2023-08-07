007ff0bd9154f44de5c34793cf23a31f9a442483
willshall <netcreaterss@gmail.com>
2023-08-07 18:46:37 +0530

    Add a sub-menu under the WooCommerce  menu

--- WordPress/products-discount/products-discount.php
+++ WordPress/products-discount/products-discount.php
@@ -6,3 +6,16 @@ Description: Products Discount Plugin
 Version: 1.0
 Author: Ravi 
 */
+
+// Add a sub-menu under the "WooCommerce" menu
+function custom_woocommerce_discounts_submenu() {
+    add_submenu_page(
+        'woocommerce',
+        'Product Discounts',
+        'Product Discounts',
+        'manage_options',
+        'custom-woocommerce-discounts',
+        'custom_woocommerce_discounts_page_callback'
+    );
+}
+add_action('admin_menu', 'custom_woocommerce_discounts_submenu');
