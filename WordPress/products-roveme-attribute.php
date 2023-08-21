fa22a98e25a5258abc6435fd775bec9cdc20431f
willshall <netcreaterss@gmail.com>
2023-08-21 19:03:37 +0530

    variable products roveme attribute from products name

---
+++ WordPress/products-roveme-attribute.php
@@ -0,0 +1,6 @@
+add_filter( 'woocommerce_product_variation_title_include_attributes', 'variation_title_not_include_attributes' );
+function variation_title_not_include_attributes( $boolean ){
+    if ( ! is_cart() )
+        $boolean = false;
+    return $boolean;
+}
