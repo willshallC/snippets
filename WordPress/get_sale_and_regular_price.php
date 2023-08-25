47fb2f89423726692551f8a6234d6947ee3b621b
willshall <netcreaterss@gmail.com>
2023-08-25 18:47:36 +0530

    get sale price

--- WordPress/get_sale_and_regular_price.php
+++ WordPress/get_sale_and_regular_price.php
@@ -1,3 +1,6 @@
 <?php
 $price = get_post_meta( get_the_ID(), '_regular_price', true);
 // $price will return regular price
+
+$sale = get_post_meta( get_the_ID(), '_sale_price', true);
+// $sale will return sale price
