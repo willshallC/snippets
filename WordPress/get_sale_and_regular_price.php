ff6a73b237c9d80e6ba96a077249f4f84ec116a3
willshall <netcreaterss@gmail.com>
2023-08-25 18:50:19 +0530

    get woocommerce products regular and sale price

--- WordPress/get_sale_and_regular_price.php
+++ WordPress/get_sale_and_regular_price.php
@@ -1,6 +1,8 @@
 <?php
-$price = get_post_meta( get_the_ID(), '_regular_price', true);
-// $price will return regular price
+    $price = get_post_meta( get_the_ID(), '_regular_price', true);
+    // $price will return regular price
 
-$sale = get_post_meta( get_the_ID(), '_sale_price', true);
-// $sale will return sale price
+    $sale = get_post_meta( get_the_ID(), '_sale_price', true);
+    // $sale will return sale price
+
+?>
