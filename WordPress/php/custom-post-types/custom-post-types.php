8d8a2cc6b54b64a95de61a5da877a541ac8159c0
willshall <netcreaterss@gmail.com>
2023-09-08 18:44:15 +0530

    Add the settings menu item to the admin dashboard

--- WordPress/php/custom-post-types/custom-post-types.php
+++ WordPress/php/custom-post-types/custom-post-types.php
@@ -7,3 +7,15 @@ Version: 1.0
 Author: Ravi
 
 */
+
+// Add the settings menu item to the admin dashboard
+function custom_post_types_menu() {
+    add_menu_page(
+        'Custom Post Types Settings',
+        'Custom Post Types',
+        'manage_options',
+        'custom-post-types-settings',
+        'custom_post_types_settings_page'
+    );
+}
+add_action('admin_menu', 'custom_post_types_menu');
