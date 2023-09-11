60c4c7f67e1adf80c0772bbfe098abd7c2914e79
willshall <netcreaterss@gmail.com>
2023-09-11 17:52:43 +0530

    Create the settings page content

--- WordPress/php/custom-post-types/custom-post-types.php
+++ WordPress/php/custom-post-types/custom-post-types.php
@@ -19,3 +19,18 @@ function custom_post_types_menu() {
     );
 }
 add_action('admin_menu', 'custom_post_types_menu');
+
+
+// Create the settings page content
+function custom_post_types_settings_page() { 
+    ?>
+    <div class="wrap">
+        <h2>Custom Post Types Settings</h2>
+        <form method="post" action="options.php">
+            <?php settings_fields('custom_post_types_settings_group'); ?>
+            <?php do_settings_sections('custom-post-types-settings'); ?>
+            <input type="submit" class="button-primary" value="Save Settings">
+        </form>
+    </div>
+    <?php
+}
