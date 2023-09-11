98100ee93e3afd790f1d8d26b18f7256d5b2a549
willshall <netcreaterss@gmail.com>
2023-09-11 17:54:55 +0530

    Register custom post types based on user input

--- WordPress/php/custom-post-types/custom-post-types.php
+++ WordPress/php/custom-post-types/custom-post-types.php
@@ -34,3 +34,29 @@ function custom_post_types_settings_page() {
     </div>
     <?php
 }
+
+// Register custom post types based on user input
+function register_custom_post_types() {
+    // Get user-defined post types from the settings
+    $custom_post_types = get_option('custom_post_types');
+
+    if (!empty($custom_post_types)) {
+        foreach ($custom_post_types as $post_type) {
+            $labels = array(
+                'name' => $post_type,
+                'singular_name' => $post_type,
+                'menu_name' => $post_type,
+            );
+
+            $args = array(
+                'labels' => $labels,
+                'public' => true,
+                'has_archive' => true,
+                'rewrite' => array('slug' => sanitize_title($post_type)),
+            );
+
+            register_post_type(sanitize_title($post_type), $args);
+        }
+    }
+}
+add_action('init', 'register_custom_post_types');
