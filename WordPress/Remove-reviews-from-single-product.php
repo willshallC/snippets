be76d0585c300325b380d5045ec127ca134be2d0
willshall <netcreaterss@gmail.com>
2023-09-22 18:57:25 +0530

    Add Reviews Below Product Gallery

--- WordPress/Remove-reviews-from-single-product.php
+++ WordPress/Remove-reviews-from-single-product.php
@@ -4,3 +4,22 @@ function woo_remove_product_tabs($tabs) {
     return $tabs; 
 }
 add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+
+// Add Reviews Below Product Gallery
+add_shortcode(
+    "wpse_comment_form",
+    function ($atts = array(), $content = "") {
+        global $product;
+      if (is_singular() && post_type_supports(get_post_type(), "comments")) {
+        ob_start(); ?>
+        <div class="product-reviews">
+        <h2 class="review-title" itemprop="headline">
+        <?php printf( __( 'Reviews (%d)', 'woocommerce' ), $product->get_review_count() ); ?>
+        </h2>
+        <?php call_user_func( 'comments_template', 999 ); ?>
+        </div>
+        <div class="clearfix clear"></div>
+        <?php return ob_get_clean();
+      }
+      return "";
+    }, 10, 2);
