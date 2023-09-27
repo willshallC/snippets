d5cb21ca3a583a2b349a8de6d9b5b371b22ea51b
willshall <netcreaterss@gmail.com>
2023-09-27 16:27:36 +0530

    Get product tags

--- WordPress/display_latest_products.php
+++ WordPress/display_latest_products.php
@@ -13,11 +13,14 @@ function display_latest_products() {
         while ($query->have_posts()) {
             $query->the_post();
             global $product;
+            // Get product tags
+            $product_tags = wp_get_post_terms(get_the_ID(), 'product_tag');
             ?>
                 <a href="<?php the_permalink(); ?>">
                     <?php the_post_thumbnail(); ?>
                     <h2><?php the_title(); ?></h2>
-                    <span class="price"><?php echo $product->get_price_html(); ?></span>                   
+                    <span class="price"><?php echo $product->get_price_html(); ?></span>
+                  
                 </a>
             <?php
         }
