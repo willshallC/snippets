ce0b27f1b585aeb2309aa1181f7d8b529c3a16ef
willshall <netcreaterss@gmail.com>
2023-09-27 16:26:10 +0530

    Query WooCommerce for the latest 3 products

---
+++ WordPress/display_latest_products.php
@@ -0,0 +1,27 @@
+<?php
+function display_latest_products() {
+    // Query WooCommerce for the latest 3 products
+    $args = array(
+        'post_type' => 'product',
+        'posts_per_page' => 3,
+        'orderby' => 'date',
+        'order' => 'DESC',
+    );
+    $query = new WP_Query($args);
+    if ($query->have_posts()) {
+        echo '<div class="latest-products">';
+        while ($query->have_posts()) {
+            $query->the_post();
+            global $product;
+            ?>
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail(); ?>
+                    <h2><?php the_title(); ?></h2>
+                    <span class="price"><?php echo $product->get_price_html(); ?></span>                   
+                </a>
+            <?php
+        }
+        echo '</div>';
+        wp_reset_postdata();
+    }
+}
