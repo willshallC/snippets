9f239f30214f50f10e4c59ebba68e2ad16789bc5
willshall <netcreaterss@gmail.com>
2023-10-09 10:03:58 +0530

    The code you've provided is for a WordPress function that displays the latest products from WooCommerce

--- WordPress/quickview/quickview.php
+++ WordPress/quickview/quickview.php
@@ -54,5 +54,27 @@ function display_latest_products() {
         'orderby' => 'date',
         'order' => 'DESC',
     );
+    $query = new WP_Query($args);
+    if ($query->have_posts()) {
+        echo '<div class="latest-products">';
+        while ($query->have_posts()) {
+            $query->the_post();
+            global $product;
+            // Get product tags
+            
+            ?>
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail(); ?>
+                    <h2><?php the_title(); ?></h2>
+                    <span class="price"><?php echo $product->get_price_html(); ?></span>
+                  
+                </a>
+            <?php
+        }
+        echo '</div>';
+        wp_reset_postdata();
+    }
+}
+
 
 }
