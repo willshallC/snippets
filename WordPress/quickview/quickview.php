2a7c84fbd5fcd0424cf9af50560d048baba415c4
willshall <netcreaterss@gmail.com>
2023-10-25 10:10:55 +0530

    Wp Query

--- WordPress/quickview/quickview.php
+++ WordPress/quickview/quickview.php
@@ -61,7 +61,119 @@ function latest_products_shortcode($attss) {
         'order' => 'DESC',
         'paged' => get_query_var('paged') ? get_query_var('paged') : 1,
     );
+
+    $products = new WP_Query($argss);
+    ob_start();
+    if ($products->have_posts()) {
+        echo '<ul class="latest-products">';
+        while ($products->have_posts()) {
+             $products->the_post();
+            $product_id = get_the_ID(); // Get the product ID
+            $permalink = get_permalink(get_the_ID());
+            echo '<li class="product">';       
+            
+			echo '<div class="product-image">';
+            $product_image = get_the_post_thumbnail();
+			
+            echo '<div class="product-image">' . $product_image . '</div>';
+			
+			echo '<span class="quick-view" data-product-id="' . $product_id . '" data-product-title="' . get_the_title() . '">Quick View</span>';
+			
+            echo '<h2>' . get_the_title() . '</h2>';
+			echo '<p>' . get_the_excerpt() . '</p>';
+			
+            $price = wc_price(get_post_meta(get_the_ID(), '_price', true));
+
+            echo '<p class="product-price">' .  $price . '</p>';
+
+           			
+			$categories = get_the_terms(get_the_ID(), 'product_cat');
+			if (!empty($categories)) {
+				
+				foreach ($categories as $category) {
+					$category_link = get_term_link($category);
+					if (!is_wp_error($category_link)) {
+						echo '<span class="category"> <strong>Category:</strong> <a href="' . esc_url($category_link) . '">' . esc_html($category->name) . '</a></span>';
+					} else {
+						echo '<span class="category">' . esc_html($category->name) . '</span>';
+					}
+				}
+				
+			}
+
+			$tags = get_the_terms(get_the_ID(), 'product_tag');
+			if (!empty($tags)) {
+				
+				foreach ($tags as $tag) {
+					$tag_link = get_term_link($tag);
+					if (!is_wp_error($tag_link)) {
+						echo '<span class="tag"><strong> Tag: </strong><a href="' . esc_url($tag_link) . '">' . esc_html($tag->name) . '</a></span>';
+					} else {
+						echo '<span class="tag">' . esc_html($tag->name) . '</span>';
+					}
+				}
+				
+			}
+			
+            echo '</li>';
+            echo '<div class="modal-box" data-product-id="' . $product_id . '">';
+            
+			echo '<div class="modal-content">';
+            echo '<div class="modal-cont-wrap">';
+			
+            			
+            echo '<div class="product-img">' . get_the_post_thumbnail() . '</div>';
+			
+            echo '<div class="modal-right-cont">';
+			echo '<span class="close">&times;</span>';
+            echo '<h2>' . get_the_title() . '</h2>';			
+			echo '<p>' . get_the_excerpt() . '</p>';
+			
+			$price = wc_price(get_post_meta(get_the_ID(), '_price', true));
+			echo '<p class="product-price">' .  $price . '</p>';
+			
+			echo '<a href="'.$permalink.'">';
+			echo '<div class="woocommerce">' . woocommerce_template_loop_add_to_cart() . '</div>'; 
+			echo '</a><br>';
+			
+						
+			$categories = get_the_terms(get_the_ID(), 'product_cat');
+			if (!empty($categories)) {
+				
+				foreach ($categories as $category) {
+					$category_link = get_term_link($category);
+					if (!is_wp_error($category_link)) {
+						echo '<span class="category"> <strong>Category:</strong> <a href="' . esc_url($category_link) . '">' . esc_html($category->name) . '</a></span>';
+					} else {
+						echo '<span class="category">' . esc_html($category->name) . '</span>';
+					}
+				}
+				
+			}
+
+			$tags = get_the_terms(get_the_ID(), 'product_tag');
+			if (!empty($tags)) {
+				
+				foreach ($tags as $tag) {
+					$tag_link = get_term_link($tag);
+					if (!is_wp_error($tag_link)) {
+						echo '<span class="tag"><strong> Tag: </strong><a href="' . esc_url($tag_link) . '">' . esc_html($tag->name) . '</a></span>';
+					} else {
+						echo '<span class="tag">' . esc_html($tag->name) . '</span>';
+					}
+				}
+				
+			}
+			
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';    
+        }
+        echo '</ul>';
+        wp_reset_postdata();
+    } 
+    return ob_get_clean();
 }
 // Register the shortcode
 add_shortcode('latest_products', 'latest_products_shortcode');
-
