0f5acb90a9f9ec2e6ca9ed7ab72af30889c9af46
willshall <netcreaterss@gmail.com>
2023-11-28 19:16:51 +0530

    Go on specific element by selecting text after the hash in the URL from url

--- jquery/hash_text.js
+++ jquery/hash_text.js
@@ -1,3 +1,14 @@
+<!-- Go on specific element by selecting text after the hash in the URL from url  -->
 jQuery(document).ready(function() {
-
+  // Get the text after the hash in the URL
+  var hashText = window.location.hash.substring(1);
+  // Check if an element with the specified 'rel' attribute exists
+  var $targetElement = jQuery('[rel="' + hashText + '"]');
+  // If the element is found, set its offset from the top to 100px
+  if ($targetElement.length > 0) {
+    var offsetTop = $targetElement.offset().top - 10;
+    jQuery('html, body').animate({
+      scrollTop: offsetTop
+    }, 1000);
+  }
 });
