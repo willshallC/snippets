61aef020a6a1a360d23872ce48cd73994b72255f
willshall <netcreaterss@gmail.com>
2023-11-29 17:54:05 +0530

    Function to toggle between Read More and Read Less

--- jquery/read_more_addition.js
+++ jquery/read_more_addition.js
@@ -1,3 +1,4 @@
+// Read more addition
 $(document).ready(function() {
     // Function to truncate text and add Read More button
     function truncateText(element, maxLength) {
@@ -16,9 +17,23 @@ $(document).ready(function() {
             element.html(content);
           }
     }
-    // Apply the function to each element with the class 'feture_body_text'
+    // Apply the function to each element with the class feture_body_text
     $('.review-bodyyy').each(function() {
       truncateText($(this), 35);
     });
-
-});
+    // Function to toggle between "Read More" and "Read Less"
+    $('.review-bodyyy').on('click', '.read-more', function(e) {
+      e.preventDefault();
+      var textContainer = $(this).closest('.review-bodyyy');
+      var fullText = textContainer.data('full-text');
+      var truncatedText = textContainer.data('truncated-text');
+      var currentText = textContainer.html();
+      if (currentText.includes('...')) {
+        // Show full content and change button text to "Read Less"
+        textContainer.html(fullText + ' <a href="#" class="read-more">Read Less</a>');
+      } else {
+        // Truncate text and change button text to "Read More"
+        textContainer.html(truncatedText + '... <a href="#" class="read-more">Read More</a>');
+      }
+    });
+  });
